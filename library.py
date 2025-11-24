076fb015aec824ee6f32a2254bf8fafd11f2551c
Aryaman Saxena <aryamansaxena1221@gmail.com>
2025-11-24 08:39:05 +0530

    library.py

--- library.py
+++ library.py
@@ -1,65 +1,74 @@
+#here i am importing book class entire data from book.py
 from book import Book
+#similarily i am importing user class data from user.py
 from user import User
+#doing the same by importing notifications from notifications.py
 from notifications import Notifications
+# and the same for reports
 from reports import Reports
+#now importing datetime module
 from datetime import datetime
+#and also importing json module 
 import json
+#and also importing os module
 import os
 
+#Now i am creating a library class
 class Library:
-    df = 'library_data.json'
+    df = 'library_data.json'#creating this file to save data later 
 
-    def __init__(self):
-        self.bks = []
-        self.usrs = []
-        self.load_data()
+    def __init__(self):#initializing the value of the library class 
+        self.bks = []#here we are creating a list which is empty and we will use to store books
+        self.usrs = []#here we are creating a list which is empty and we will use it store user data
+        self.load_data()#we use this method to get any data which we have
 
-    def addb(self, bk):
-        self.bks.append(bk)
+    def addb(self, bk):#we are adding books by using this method
+        self.bks.append(bk)#it will append that book in the list
 
-    def addu(self, usr):
-        self.usrs.append(usr)
+    def addu(self, usr):#we are using it to add a new user
+        self.usrs.append(usr)#we will append it in the list 
 
-    def showb(self):
-        if not self.bks:
+    def showb(self):#defining this method to show the books
+        if not self.bks:#if a book is not in the list then print that no book is found
             print("No books found.")
             return
-        print("Books in Library:")
+        print("Books in Library:")#otherwise print that books are in library
         for bk in self.bks:
-            bk.display()
+            bk.display()#display entire information of the books
 
-    def showu(self):
-        if not self.usrs:
+    def showu(self):# it shows the list of all the users
+        if not self.usrs:#if somebody is not registered then print that the user is not registered 
             print("No users registered.")
             return
-        print("Registered Users:")
-        for usr in self.usrs:
-            print("ID:", usr.user_id, ",Name:", usr.name, ",Role:", usr.role)
+        print("Registered Users:")#otherwise print that the user is registered 
+        for usr in self.usrs:#using loops
+            print("ID:", usr.user_id, ",Name:", usr.name, ",Role:", usr.role)#now print the entire information of teh user
 
-    def gbi(self, bid):
-        for bk in self.bks:
-            if bk.book_id == bid:
+    def gbi(self, bid):#here we are finding by it's id
+        for bk in self.bks:#using loop to check every book
+            if bk.book_id == bid:#if the id is mathced 
                 return bk
-        return None
+        return None#then return the book otherwise not 
 
-    def gui(self, uid):
-        for usr in self.usrs:
-            if usr.user_id == uid:
+    def gui(self, uid):#now doing the same thing for the user.
+        for usr in self.usrs:#the loop will check if the user is present in the user list
+            if usr.user_id == uid:#if id matched then return the user otherwise return none
                 return usr
         return None
 
-    def show_pop_bks(self):
-        if not self.bks:
+    def show_pop_bks(self):#this method is used to display popular books
+        if not self.bks:#it is used to check whether the book list is empty or not 
             print("No books to show.")
             return
-        sorted_bks = sorted(self.bks, key=lambda b: b.times_borrowed, reverse=True)
+        sorted_bks = sorted(self.bks, key=lambda b: b.times_borrowed, reverse=True)#otherwise sort the list based on how many times the book has been borrowed 
         print("Top 5 Popular Books:")
         for bk in sorted_bks[:5]:
-            print(bk.title, "- Borrowed", bk.times_borrowed, "times")
+            print(bk.title, "- Borrowed", bk.times_borrowed, "times")#and then print till 0 to 4th index so basically top 5
 
     def saved(self):
-        data = {'bks': [], 'usrs': []}
-        for bk in self.bks:
+        #we will now save the data
+        data = {'bks': [], 'usrs': []}#we are creating a dictionary to set the data in the json file
+        for bk in self.bks:#creating a loop for all books objects 
             data['bks'].append({'book_id': bk.book_id,
                                 'title': bk.title,
                                 'author': bk.author,
@@ -67,7 +76,7 @@ class Library:
                                 'price': bk.price,
                                 'mbd': bk.max_borrow_days,
                                 'times_borrowed': bk.times_borrowed})
-        for usr in self.usrs:
+        for usr in self.usrs:#creating a loop for all the users.
             brw = {}
             for bid, (bk, dd) in usr.brw_bks.items():
                 brw[str(bid)] = dd.strftime('%Y-%m-%d')
@@ -75,19 +84,19 @@ class Library:
                                  'name': usr.name,
                                  'role': usr.role,
                                  'borrowed_books': brw})
-        try:
+        try:#using try and except for error handling 
             with open(self.df, 'w') as f:
                 json.dump(data, f, indent=4)
-            print("Data saved.")
+            print("Data saved.")#the data is saved 
         except Exception as e:
             print("Error saving:", e)
 
     def load_data(self):
-        if not os.path.exists(self.df):
+        if not os.path.exists(self.df):#if the path of the does not exists then exit
             return
         try:
-            with open(self.df, 'r') as f:
-                data = json.load(f)
+            with open(self.df, 'r') as f:#otherwise open it in read mode as f 
+                data = json.load(f)#and load it 
             self.bks = []
             for bkdata in data.get('bks', []):
                 bk = Book(
@@ -117,10 +126,11 @@ class Library:
         except Exception as e:
             print("Error loading:", e)
 
-def main():
-    lib = Library()
+def main():#now definining the main function 
+    lib = Library()#creating lib as the object 
     while True:
-        Notifications.due_date(lib.usrs)
+        #running an infinite loop
+        Notifications.due_date(lib.usrs)#addinig data of due date from notifications 
         print("Library Menu")
         print("1. Show books")
         print("2. Borrow book")
@@ -132,75 +142,76 @@ def main():
         print("8. Add user")
         print("9. Show users")
         print("10. Save & exit")
+        #these are all the options present 
         choice = input("Choose option: ")
-        if choice == '1':
+        if choice == '1':#if options 1 is choosed then print all the available books 
             lib.showb()
-        elif choice == '2':
+        elif choice == '2':#if 2nd choice is taken 
             try:
-                uid = int(input("User ID: "))
+                uid = int(input("User ID: "))#then ask the user id, the book id and how many days u want to borrow 
                 bid = int(input("Book ID: "))
                 days = int(input("Days to borrow: "))
             except ValueError:
-                print("Please enter valid number")
+                print("Please enter valid number")#for error handling if a valid number is not entered 
                 continue
-            usr = lib.gui(uid)
-            bk = lib.gbi(bid)
-            if usr and bk:
+            usr = lib.gui(uid)#check the user by his id
+            bk = lib.gbi(bid)#check the boook by the id 
+            if usr and bk:# if they exist then the boook is borrowed 
                 usr.borrow_book(bk, days)
             else:
-                print("User or book not found")
+                print("User or book not found")#otherwise print the error message 
         elif choice == '3':
             try:
-                uid = int(input("User ID: "))
+                uid = int(input("User ID: "))#enter the id and book id 
                 bid = int(input("Book ID: "))
             except ValueError:
-                print("Please enter valid number")
+                print("Please enter valid number")#once again message for error
                 continue
-            usr = lib.gui(uid)
+            usr = lib.gui(uid)#check user id and book id 
             bk = lib.gbi(bid)
             if usr and bk:
-                usr.return_book(bk)
+                usr.return_book(bk)#if both correct then the return book
             else:
                 print("User or book not found")
         elif choice == '4':
             try:
-                uid = int(input("User ID: "))
+                uid = int(input("User ID: "))#ask user id 
             except ValueError:
-                print("Invalid ID")
+                print("Invalid ID")#otherwise print error statement 
                 continue
-            usr = lib.gui(uid)
+            usr = lib.gui(uid)#check his id 
             if usr:
-                usr.view_borrowed_books()
+                usr.view_borrowed_books()#display the borrowed books list 
             else:
                 print("User not found")
         elif choice == '5':
-            lib.show_pop_bks()
+            lib.show_pop_bks()#then to show popular books which are top 5
         elif choice == '6':
-            Reports.obr(lib.usrs)
+            Reports.obr(lib.usrs)#then to show overdue books reports click 6 
         elif choice == '7':
-            Reports.ubs(lib.usrs)
+            Reports.ubs(lib.usrs)# click 7 for checking borrow summary of user
         elif choice == '8':
             try:
-                new_uid = int(input("New user ID: "))
+                new_uid = int(input("New user ID: "))#entering new user id 
                 if lib.gui(new_uid):
-                    print("User ID taken")
+                    print("User ID taken")#if id already there 
                     continue
-                name = input("Name: ")
-                role = input("Role (Admin/Member): ")
+                name = input("Name: ")#otherwise enter name
+                role = input("Role (Admin/Member): ")#enter your role also
                 if role not in ("Admin", "Member"):
-                    role = "Member"
-                lib.addu(User(new_uid, name, role))
-                print(name, "with user ID", new_uid, "added as", role)
+                    role = "Member"#i am setting default role as member 
+                lib.addu(User(new_uid, name, role))#add this data is addu mmethod 
+                print(name, "with user ID", new_uid, "added as", role)#print the required information 
             except ValueError:
-                print("Invalid input")
+                print("Invalid input")#otherwise print invalid input 
         elif choice == '9':
-            lib.showu()
+            lib.showu()#show the list of all the users
         elif choice == '10':
             lib.saved()
-            print("Data is saved")
+            print("Data is saved")#and save the data 
             break
         else:
-            print("Only choose between 1 to 10")
+            print("Only choose between 1 to 10")# error message if somebody uses other option 
 
 if __name__ == "__main__":
     main()
